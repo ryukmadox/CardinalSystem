8cc3b4b1fa36138bbe87dd93e88d721bd94d8b9f
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-02-06 17:12:14 +0530

    Update tree.py

--- Sibyl_System/plugins/Mongo_DB/tree.py
+++ Sibyl_System/plugins/Mongo_DB/tree.py
@@ -12,24 +12,24 @@ async def get_data() -> dict:
 
 async def add_developer(cardinal: int, developer: int) -> True:
     data = await get_data()
-    data["data"][str(sibyl)][str(inspector)] = []
-    data["standalone"][str(inspector)] = {
-        "addedby": sibyl,
+    data["data"][str(cardinal)][str(developer)] = []
+    data["standalone"][str(developer)] = {
+        "addedby": cardinal,
         "timestamp": datetime.timestamp(datetime.now()),
     }
     await db.replace_one(await get_data(), data)
 
 
-async def add_manager(developer: int, developer: int) -> True:
+async def add_manager(developer: int, manager: int) -> True:
     data = await get_data()
-    sibyl = data["standalone"][str(inspector)]["addedby"]
+    cardinal = data["standalone"][str(developer)]["addedby"]
     if sibyl == 777000:
-        s = data["data"][str(inspector)]
-        s[list(choice(s.keys()))].append([enforcer])
+        s = data["data"][str(developer)]
+        s[list(choice(s.keys()))].append([manager])
     else:
-        data["data"][str(sibyl)][str(inspector)].append([enforcer])
-    data["standalone"][str(enforcer)] = {
-        "addedby": inspector,
+        data["data"][str(cardinal)][str(developer)].append([manager])
+    data["standalone"][str(manager)] = {
+        "addedby": developer,
         "timestamp": datetime.timestamp(datetime.now()),
     }
     await db.replace_one(await get_data(), data)
